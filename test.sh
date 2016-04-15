44bc0821401fb190a5ccb4abd0a926fe0e69cf30
Alan Gardner <alanctgardner@gmail.com>
2016-04-15 17:00:42 -0400

    Add test for encoding nested records

--- test.sh
+++ test.sh
@@ -7,9 +7,11 @@ $GOPATH/bin/gogen-avro test/primitive/primitives.avsc > test/primitive/schema.go
 $GOPATH/bin/gogen-avro test/maps/maps.avsc > test/maps/schema.go
 $GOPATH/bin/gogen-avro test/arrays/arrays.avsc > test/arrays/schema.go
 $GOPATH/bin/gogen-avro test/union/union.avsc > test/union/schema.go
+$GOPATH/bin/gogen-avro test/nested/nested.avsc > test/nested/schema.go
 
 # Run the unit tests to round-trip data with this schema
 go test -bench=. github.com/alanctgardner/gogen-avro/test/primitive
 go test -bench=. github.com/alanctgardner/gogen-avro/test/maps
 go test -bench=. github.com/alanctgardner/gogen-avro/test/arrays
 go test -bench=. github.com/alanctgardner/gogen-avro/test/union
+go test -bench=. github.com/alanctgardner/gogen-avro/test/nested
---
+++ test/nested/schema.go
@@ -0,0 +1,236 @@
+package avro
+
+import "io"
+import "math"
+
+type NestedTestRecord struct {
+	NumberField NumberRecord
+	OtherField  NestedRecord
+}
+
+func (r NestedTestRecord) Serialize(w io.Writer) error {
+	return writeNestedTestRecord(r, w)
+}
+
+type ByteWriter interface {
+	Grow(int)
+	WriteByte(byte) error
+}
+
+type NestedRecord struct {
+	StringField string
+	BoolField   bool
+	BytesField  []byte
+}
+
+func (r NestedRecord) Serialize(w io.Writer) error {
+	return writeNestedRecord(r, w)
+}
+
+type NumberRecord struct {
+	IntField    int32
+	LongField   int64
+	FloatField  float32
+	DoubleField float64
+}
+
+func (r NumberRecord) Serialize(w io.Writer) error {
+	return writeNumberRecord(r, w)
+}
+
+type StringWriter interface {
+	WriteString(string) (int, error)
+}
+
+func encodeFloat(w io.Writer, byteCount int, bits uint64) error {
+	var err error
+	var bb []byte
+	bw, ok := w.(ByteWriter)
+	if ok {
+		bw.Grow(byteCount)
+	} else {
+		bb = make([]byte, 0, byteCount)
+	}
+	for i := 0; i < byteCount; i++ {
+		if bw != nil {
+			err = bw.WriteByte(byte(bits & 255))
+			if err != nil {
+				return err
+			}
+		} else {
+			bb = append(bb, byte(bits&255))
+		}
+		bits = bits >> 8
+	}
+	if bw == nil {
+		_, err = w.Write(bb)
+		return err
+	}
+	return nil
+}
+
+func encodeInt(w io.Writer, byteCount int, encoded uint64) error {
+	var err error
+	var bb []byte
+	bw, ok := w.(ByteWriter)
+	// To avoid reallocations, grow capacity to the largest possible size
+	// for this integer
+	if ok {
+		bw.Grow(byteCount)
+	} else {
+		bb = make([]byte, 0, byteCount)
+	}
+
+	if encoded == 0 {
+		if bw != nil {
+			err = bw.WriteByte(0)
+			if err != nil {
+				return err
+			}
+		} else {
+			bb = append(bb, byte(0))
+		}
+	} else {
+		for encoded > 0 {
+			b := byte(encoded & 127)
+			encoded = encoded >> 7
+			if !(encoded == 0) {
+				b |= 128
+			}
+			if bw != nil {
+				err = bw.WriteByte(b)
+				if err != nil {
+					return err
+				}
+			} else {
+				bb = append(bb, b)
+			}
+		}
+	}
+	if bw == nil {
+		_, err := w.Write(bb)
+		return err
+	}
+	return nil
+
+}
+
+func writeBool(r bool, w io.Writer) error {
+	var b byte
+	if r {
+		b = byte(1)
+	}
+
+	var err error
+	if bw, ok := w.(ByteWriter); ok {
+		err = bw.WriteByte(b)
+	} else {
+		bb := make([]byte, 1)
+		bb[0] = b
+		_, err = w.Write(bb)
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func writeBytes(r []byte, w io.Writer) error {
+	err := writeLong(int64(len(r)), w)
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(r)
+	return err
+}
+
+func writeDouble(r float64, w io.Writer) error {
+	bits := uint64(math.Float64bits(r))
+	const byteCount = 8
+	return encodeFloat(w, byteCount, bits)
+}
+
+func writeFloat(r float32, w io.Writer) error {
+	bits := uint64(math.Float32bits(r))
+	const byteCount = 4
+	return encodeFloat(w, byteCount, bits)
+}
+
+func writeInt(r int32, w io.Writer) error {
+	downShift := uint32(31)
+	encoded := uint64((uint32(r) << 1) ^ uint32(r>>downShift))
+	const maxByteSize = 5
+	return encodeInt(w, maxByteSize, encoded)
+}
+
+func writeLong(r int64, w io.Writer) error {
+	downShift := uint64(63)
+	encoded := uint64((r << 1) ^ (r >> downShift))
+	const maxByteSize = 10
+	return encodeInt(w, maxByteSize, encoded)
+}
+
+func writeNestedRecord(r NestedRecord, w io.Writer) error {
+	var err error
+	err = writeString(r.StringField, w)
+	if err != nil {
+		return err
+	}
+	err = writeBool(r.BoolField, w)
+	if err != nil {
+		return err
+	}
+	err = writeBytes(r.BytesField, w)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+func writeNestedTestRecord(r NestedTestRecord, w io.Writer) error {
+	var err error
+	err = writeNumberRecord(r.NumberField, w)
+	if err != nil {
+		return err
+	}
+	err = writeNestedRecord(r.OtherField, w)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+func writeNumberRecord(r NumberRecord, w io.Writer) error {
+	var err error
+	err = writeInt(r.IntField, w)
+	if err != nil {
+		return err
+	}
+	err = writeLong(r.LongField, w)
+	if err != nil {
+		return err
+	}
+	err = writeFloat(r.FloatField, w)
+	if err != nil {
+		return err
+	}
+	err = writeDouble(r.DoubleField, w)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func writeString(r string, w io.Writer) error {
+	err := writeLong(int64(len(r)), w)
+	if err != nil {
+		return err
+	}
+	if sw, ok := w.(StringWriter); ok {
+		_, err = sw.WriteString(r)
+	} else {
+		_, err = w.Write([]byte(r))
+	}
+	return err
+}
