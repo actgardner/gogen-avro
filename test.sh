995b999807267574d0f8b54af6b521713db7adee
Alan Gardner <alanctgardner@gmail.com>
2016-12-31 11:30:48 -0500

    Add option to run only one integration test

--- test.sh
+++ test.sh
@@ -2,11 +2,19 @@
 
 set -e
 
+run_test() {
+	echo "Running test $t"
+	go generate -v github.com/alanctgardner/gogen-avro/$1
+	go get -t -v github.com/alanctgardner/gogen-avro/$1
+	go test -v github.com/alanctgardner/gogen-avro/$1
+}
+
 go install github.com/alanctgardner/gogen-avro
 
-for t in test/*/; do
-	echo "Running test $t"
-	go generate -v github.com/alanctgardner/gogen-avro/$t
-	go get -t -v github.com/alanctgardner/gogen-avro/$t
-	go test  github.com/alanctgardner/gogen-avro/$t
-done
+if [ $# -eq 0 ]; then
+	for t in test/*/; do
+		run_test $t	
+	done
+else
+	run_test test/$1
+fi
