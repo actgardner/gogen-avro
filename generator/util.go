e15a1297934be9e284860655ffd3b44871ad8679
Ricardo Domingos <ricardohsd@gmail.com>
2018-10-06 15:43:48 +0200

    Improve test coverage (#59)
    * Use golang comment format
    
    * Add tests for ToPublicName and ToSnake
    
    ToSnake test based on https://gist.github.com/elwinar/14e1e897fdbe4d3432e1
    
    * Removed unnused concatSortedMap function
    
    * Change code documentation to follow golang format

--- generator/util.go
+++ generator/util.go
@@ -1,33 +1,17 @@
 package generator
 
 import (
-	"sort"
 	"strings"
 	"unicode"
 )
 
-/*
-  ToPublicName returns a go-idiomatic public name. The Avro spec specifies names must start with [A-Za-z_] and contain [A-Za-z0-9_].
-  The golang spec says valid identifiers start with [A-Za-z_] and contain [A-Za-z0-9], but the first character must be [A-Z] for the field to be public.
-*/
+// ToPublicName returns a go-idiomatic public name. The Avro spec specifies names must start with [A-Za-z_] and contain [A-Za-z0-9_].
+// The golang spec says valid identifiers start with [A-Za-z_] and contain [A-Za-z0-9], but the first character must be [A-Z] for the field to be public.
 func ToPublicName(name string) string {
 	return strings.Title(strings.Trim(name, "_"))
 }
 
-func concatSortedMap(m map[string]string, sep string) string {
-	keys := make([]string, 0)
-	for k := range m {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-	s := ""
-	for _, k := range keys {
-		s += m[k] + sep
-	}
-	return s
-}
-
-// Make filenames snake-case, taken from https://gist.github.com/elwinar/14e1e897fdbe4d3432e1
+// ToSnake makes filenames snake-case, taken from https://gist.github.com/elwinar/14e1e897fdbe4d3432e1
 func ToSnake(in string) string {
 	runes := []rune(in)
 	length := len(runes)
