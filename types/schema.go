ab09904fcbb409d872e9eca951de0ef8fa4ae29f
Jeroen Rinzema <post@jeroen.no>
2019-03-18 19:48:56 +0100

    refactoring: renamed tags property to go.tags

--- types/schema.go
+++ types/schema.go
@@ -196,10 +196,10 @@ func (n *Namespace) decodeRecordDefinition(namespace string, schemaMap map[strin
 		}
 
 		var fieldTags string
-		if tags, ok := field["tags"]; ok {
+		if tags, ok := field["go.tags"]; ok {
 			fieldTags, ok = tags.(string)
 			if !ok {
-				return nil, NewWrongMapValueTypeError("tags", "string", tags)
+				return nil, NewWrongMapValueTypeError("go.tags", "string", tags)
 			}
 		}
 
