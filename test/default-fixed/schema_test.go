99aad7ac8159c69a0b84422571a7ef202df9827d
Rajaram Gaunker <rajaram@neeva.co>
2019-09-15 12:49:49 -0700

    Add relevant test

--- test/default-fixed/schema_test.go
+++ test/default-fixed/schema_test.go
@@ -1,62 +1,17 @@
 package avro
 
 import (
-	"bytes"
-	"io/ioutil"
-	"reflect"
 	"testing"
 
-	"github.com/linkedin/goavro"
 	"github.com/stretchr/testify/assert"
 )
 
-// Round-trip some primitive values through our serializer and goavro to verify
-var fixtures = []FixedDefaultTestRecord{
-	{
-		FixedField: [12]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	},
-	{
-		FixedField: [12]byte{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255},
-	},
-	{
-		FixedField: [12]byte{0, 1, 3, 7, 15, 31, 63, 127, 255, 0, 2, 128},
-	},
-}
+func TestDefault(t *testing.T) {
+	var fixedDefault = NewFixedDefaultTestRecord()
+	fixedDefault.SetDefault(0)
 
-func TestFixedFixture(t *testing.T) {
-	schemaJson, err := ioutil.ReadFile("fixed-default.avsc")
-	assert.Nil(t, err)
-
-	codec, err := goavro.NewCodec(string(schemaJson))
-	assert.Nil(t, err)
-
-	var buf bytes.Buffer
-	for _, f := range fixtures {
-		buf.Reset()
-		err = f.Serialize(&buf)
-		assert.Nil(t, err)
-
-		datum, _, err := codec.NativeFromBinary(buf.Bytes())
-		assert.Nil(t, err)
-
-		record := datum.(map[string]interface{})
-		recordVal, ok := record["FixedField"]
-		assert.Equal(t, true, ok)
-		if !reflect.DeepEqual(recordVal, ([]byte)((f.FixedField)[:])) {
-			t.Fatalf("FixedField %v is not equal to %v", recordVal.([]byte), ([]byte)((f.FixedField)[:]))
-		}
+	expected := FixedDefaultTestRecord{
+		FixedField: [12]byte{0, 1, 18, 0, 19, 67, 0, 1, 18, 0, 19, 83},
 	}
-}
-
-func TestRoundTrip(t *testing.T) {
-	var buf bytes.Buffer
-	for _, f := range fixtures {
-		buf.Reset()
-		err := f.Serialize(&buf)
-		assert.Nil(t, err)
-
-		datum, err := DeserializeFixedDefaultTestRecord(&buf)
-		assert.Nil(t, err)
-		assert.Equal(t, *datum, f)
-	}
+	assert.Equal(t, expected, fixedDefault, "Comparing default value")
 }
