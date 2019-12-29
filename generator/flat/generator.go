81a390118e5d842a446a23b95025082c86732b95
A Gardner <3100188+actgardner@users.noreply.github.com>
2019-12-29 17:54:39 -0500

    WIP move generator to new package

---
+++ generator/flat/generator.go
@@ -0,0 +1,10 @@
+package flat
+
+// FlatPackageGenerator emits a file per generated type, all in a single paGo package without handling namespacing
+type FlatPackageGenerator struct {
+	containers bool
+}
+
+func AddDefinition(def avro.Definition) error {
+
+}
