ac91f5b2d963f9035b02fe886ae8d3b610659da5
A Gardner <3100188+actgardner@users.noreply.github.com>
2020-01-20 18:25:25 -0500

    Simplify test script

--- test.sh
+++ test.sh
@@ -1,20 +1,7 @@
-#!/bin/bash -x
+#!/bin/bash -ex
 
-set -e
+go install ./cmd/...
 
-run_test() {
-	echo "Running test $t"
-	go generate -v github.com/actgardner/gogen-avro/$1
-	go get -t -v github.com/actgardner/gogen-avro/$1
-	go test -v github.com/actgardner/gogen-avro/$1
-}
-
-go install github.com/actgardner/gogen-avro/cmd/...
-
-if [ $# -eq 0 ]; then
-	for t in test/*/; do
-		run_test $t	
-	done
-else
-	run_test test/$1
-fi
+go generate ./...
+go get -t -v -u ./...
+go test ./...
