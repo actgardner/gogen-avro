9942f648cc56c1a5c66d0afe8f0e970545aafc9d
Daniel M. Lambea <dmlambea@gmail.com>
2020-02-29 12:00:46 +0000

    Implement (EXIT, NULL) in evaluator
    'evalInner' now returns a boolean flag 'accepted' to indicate the caller if the read data is accepted or not for optional fields. If accepted is false, then the data must be discarded. This flag is honored only in the ENTER and APPENDMAP instructions, since currently only unions and map values can be nillable. Data is discarded in records by calling its Clear method. Maps are cleared by calling its ClearMap method.
    
    Minor changes:
      - removed unnecessary 'break' from switches.
      - added '// Do nothing' comment in Null operation to make it clear it is empty code.
      - removed global err variable.

--- vm/eval.go
+++ vm/eval.go
@@ -27,10 +27,11 @@ func Eval(r io.Reader, program *Program, target types.Field) (err error) {
 		}
 	}()
 
-	return evalInner(r, program, target, &pc)
+	_, err = evalInner(r, program, target, &pc)
+	return err
 }
 
-func evalInner(r io.Reader, program *Program, target types.Field, pc *int) (err error) {
+func evalInner(r io.Reader, program *Program, target types.Field, pc *int) (bool, error) {
 	var loop int64
 
 	frame := stackFrame{}
@@ -38,144 +39,119 @@ func evalInner(r io.Reader, program *Program, target types.Field, pc *int) (err 
 		inst := program.Instructions[*pc]
 		switch inst.Op {
 		case Read:
+			var err error
 			switch inst.Operand {
 			case Null:
-				break
+				// Do nothing
 			case Boolean:
 				frame.Boolean, err = readBool(r)
-				break
 			case Int:
 				frame.Int, err = readInt(r)
-				break
 			case Long:
 				frame.Long, err = readLong(r)
-				break
 			case UnusedLong:
 				_, err = readLong(r)
-				break
 			case Float:
 				frame.Float, err = readFloat(r)
-				break
 			case Double:
 				frame.Double, err = readDouble(r)
-				break
 			case Bytes:
 				frame.Bytes, err = readBytes(r)
-				break
 			case String:
 				frame.String, err = readString(r)
-				break
 			default:
 				frame.Bytes, err = readFixed(r, inst.Operand-11)
-				break
 			}
-			break
+			if err != nil {
+				return false, err
+			}
 		case Set:
 			switch inst.Operand {
 			case Null:
-				break
+				// Do nothing
 			case Boolean:
 				target.SetBoolean(frame.Boolean)
-				break
 			case Int:
 				target.SetInt(frame.Int)
-				break
 			case Long:
 				target.SetLong(frame.Long)
-				break
 			case Float:
 				target.SetFloat(frame.Float)
-				break
 			case Double:
 				target.SetDouble(frame.Double)
-				break
 			case Bytes:
 				target.SetBytes(frame.Bytes)
-				break
 			case String:
 				target.SetString(frame.String)
-				break
 			}
-			break
 		case SetDefault:
 			target.SetDefault(inst.Operand)
-			break
 		case Enter:
 			*pc += 1
-			if err = evalInner(r, program, target.Get(inst.Operand), pc); err != nil {
-				return err
+			if accepted, err := evalInner(r, program, target.Get(inst.Operand), pc); err != nil {
+				return false, err
+			} else if !accepted {
+				target.Clear(inst.Operand)
 			}
-			break
 		case Exit:
-			target.Finalize()
-			return nil
+			if inst.Operand != Null {
+				target.Finalize()
+			}
+			return inst.Operand != Null, nil
 		case AppendArray:
 			*pc += 1
-			if err = evalInner(r, program, target.AppendArray(), pc); err != nil {
-				return err
+			if _, err := evalInner(r, program, target.AppendArray(), pc); err != nil {
+				return false, err
 			}
-			break
 		case AppendMap:
 			*pc += 1
-			if err = evalInner(r, program, target.AppendMap(frame.String), pc); err != nil {
-				return err
+			if accepted, err := evalInner(r, program, target.AppendMap(frame.String), pc); err != nil {
+				return false, err
+			} else if !accepted {
+				target.ClearMap(frame.String)
 			}
-			break
 		case Call:
 			curr := *pc
 			*pc = inst.Operand
-			if err = evalInner(r, program, target, pc); err != nil {
-				return err
+			if _, err := evalInner(r, program, target, pc); err != nil {
+				return false, err
 			}
 			*pc = curr
-			break
 		case Return:
-			return nil
+			return true, nil
 		case Jump:
 			*pc = inst.Operand - 1
-			break
 		case EvalGreater:
 			frame.Condition = (frame.Long > int64(inst.Operand))
-			break
 		case EvalEqual:
 			frame.Condition = (frame.Long == int64(inst.Operand))
-			break
 		case CondJump:
 			if frame.Condition {
 				*pc = inst.Operand - 1
 			}
-			break
 		case AddLong:
 			frame.Long += int64(inst.Operand)
-			break
 		case SetLong:
 			frame.Long = int64(inst.Operand)
-			break
 		case MultLong:
 			frame.Long *= int64(inst.Operand)
-			break
 		case PushLoop:
 			loop = frame.Long
 			*pc += 1
-			if err = evalInner(r, program, target, pc); err != nil {
-				return err
+			if _, err := evalInner(r, program, target, pc); err != nil {
+				return false, err
 			}
 			frame.Long = loop
-			break
 		case PopLoop:
-			return nil
+			return true, nil
 		case Halt:
 			if inst.Operand == 0 {
-				return nil
+				return true, nil
 			}
-			return fmt.Errorf("Runtime error: %v, frame: %v, pc: %v", program.Errors[inst.Operand-1], frame, pc)
+			return false, fmt.Errorf("Runtime error: %v, frame: %v, pc: %v", program.Errors[inst.Operand-1], frame, pc)
 		default:
-			return fmt.Errorf("Unknown instruction %v", program.Instructions[*pc])
-		}
-
-		if err != nil {
-			return err
+			return false, fmt.Errorf("Unknown instruction %v", program.Instructions[*pc])
 		}
 	}
-	return nil
+	return true, nil
 }
