0ab0bd7ded417e4130106b9922c967b80885bff5
Daniel M. Lambea <dmlambea@gmail.com>
2020-02-29 12:42:05 +0000

    Meta map is no longer a pointer in AvroContainerHeader

--- container/writer.go
+++ container/writer.go
@@ -79,7 +79,7 @@ func NewWriter(writer io.Writer, codec Codec, recordsPerBlock int64, schema stri
 func (avroWriter *Writer) writeHeader(schema string) error {
 	header := &avro.AvroContainerHeader{
 		Magic: [4]byte{'O', 'b', 'j', 1},
-		Meta: &avro.MapBytes{M: map[string][]byte{
+		Meta: avro.MapBytes{M: map[string][]byte{
 			"avro.schema": []byte(schema),
 			"avro.codec":  []byte(avroWriter.codec),
 		}},
