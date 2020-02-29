11e9d6290afb1b4bdb5ee039c300a0d70cb594ac
Daniel M. Lambea <dmlambea@gmail.com>
2020-02-29 12:21:09 +0000

    Implement optional types in records
    'Get(i int)' method takes into account non-optional fields, so they are returned as pointers.
    
    Implement valid Clear method for optional fields only. Implement unsupported ClearMap.
    
    Minor changes:
      - No need for constructor method. All records are generated using idiomatic Go code.
      - Struct code is generated using GoType instead of Name.
      - Reduced boilerplate in Deserialize methods.
      - All methods are pointer receivers, like in all other types.

--- generator/flat/templates/record.go
+++ generator/flat/templates/record.go
@@ -3,17 +3,15 @@ package templates
 const RecordTemplate = `
 import (
 	"io"
+	
 	"github.com/actgardner/gogen-avro/vm/types"
 	"github.com/actgardner/gogen-avro/vm"
 	"github.com/actgardner/gogen-avro/compiler"
 )
 
 {{ if ne .Doc "" }}// {{ .Doc}}{{ end }}  
-type {{ .Name }} struct {
-{{ range $i, $field := .Fields }}
-	{{ if ne $field.Doc "" }}
-	// {{ $field.Doc }}
-	{{ end }}
+type {{ .GoType }} struct { {{ range $i, $field := .Fields }}
+	{{ if ne $field.Doc "" }}// {{ $field.Doc }}{{ end }}
 	{{ if ne $field.Tags "" }}
 		{{ $field.SimpleName }} {{ $field.Type.GoType }} ` + "`{{ $field.Tags }}`" + `
 	{{ else }}
@@ -22,37 +20,20 @@ type {{ .Name }} struct {
 {{ end }}
 }
 
-func {{ .ConstructorMethod }} ({{ .GoType}}) {
-	return &{{ .Name }}{}
-}
-
-func Deserialize{{ .Name }}(r io.Reader) ({{ .GoType }}, error) {
-	t := {{ .ConstructorMethod }}
+func Deserialize{{ .GoType }}(r io.Reader) (t {{ .GoType }}, err error) {
 	deser, err := compiler.CompileSchemaBytes([]byte(t.Schema()), []byte(t.Schema()))
-	if err != nil {
-		return nil, err
+	if err == nil {
+		err = vm.Eval(r, deser, &t)
 	}
-
-	err = vm.Eval(r, deser, t)
-	if err != nil {
-		return nil, err	
-	}
-	return t, err
+	return
 }
 
-func Deserialize{{ .Name }}FromSchema(r io.Reader, schema string) ({{ .GoType }}, error) {
-	t := {{ .ConstructorMethod }}
-
+func Deserialize{{ .GoType }}FromSchema(r io.Reader, schema string) (t {{ .GoType }}, err error) {
 	deser, err := compiler.CompileSchemaBytes([]byte(schema), []byte(t.Schema()))
-	if err != nil {
-		return nil, err
+	if err == nil {
+		err = vm.Eval(r, deser, &t)
 	}
-
-	err = vm.Eval(r, deser, t)
-	if err != nil {
-		return nil, err	
-	}
-	return t, err
+	return
 }
 
 func {{ .SerializerMethod }}(r {{ .GoType }}, w io.Writer) error {
@@ -78,46 +59,54 @@ func (r {{ .GoType }}) SchemaName() string {
 	return {{ printf "%q" .AvroName.String }}
 }
 
-func (_ {{ .GoType }}) SetBoolean(v bool) { panic("Unsupported operation") }
-func (_ {{ .GoType }}) SetInt(v int32) { panic("Unsupported operation") }
-func (_ {{ .GoType }}) SetLong(v int64) { panic("Unsupported operation") }
-func (_ {{ .GoType }}) SetFloat(v float32) { panic("Unsupported operation") }
-func (_ {{ .GoType }}) SetDouble(v float64) { panic("Unsupported operation") }
-func (_ {{ .GoType }}) SetBytes(v []byte) { panic("Unsupported operation") }
-func (_ {{ .GoType }}) SetString(v string) { panic("Unsupported operation") }
-func (_ {{ .GoType }}) SetUnionElem(v int64) { panic("Unsupported operation") }
+func (_ *{{ .GoType }}) SetBoolean(v bool) { panic("Unsupported operation") }
+func (_ *{{ .GoType }}) SetInt(v int32) { panic("Unsupported operation") }
+func (_ *{{ .GoType }}) SetLong(v int64) { panic("Unsupported operation") }
+func (_ *{{ .GoType }}) SetFloat(v float32) { panic("Unsupported operation") }
+func (_ *{{ .GoType }}) SetDouble(v float64) { panic("Unsupported operation") }
+func (_ *{{ .GoType }}) SetBytes(v []byte) { panic("Unsupported operation") }
+func (_ *{{ .GoType }}) SetString(v string) { panic("Unsupported operation") }
+func (_ *{{ .GoType }}) SetUnionElem(v int64) { panic("Unsupported operation") }
 
-func (r {{ .GoType }}) Get(i int) types.Field {
+func (r *{{ .GoType }}) Get(i int) types.Field {
 	switch (i) {
 	{{ range $i, $field := .Fields }}
 	case {{ $i }}:
 		{{ if $.ConstructableForField $field | ne "" }}
-			{{ $.ConstructableForField $field }}	
+			{{ $.ConstructableForField $field }}
 		{{ end }}
 		{{ if ne $field.Type.WrapperType "" }}
 			return (*{{ $field.Type.WrapperType }})(&r.{{ $field.GoName }})
 		{{ else }}
-			return r.{{ $field.GoName }}
+			{{ if $field.Type.IsOptional }}return r.{{ $field.GoName }}{{ else }}return &r.{{ $field.GoName }}{{ end }}
 		{{ end }}
 	{{ end }}
+	default:
+		panic("Unknown field index")
 	}
-	panic("Unknown field index")
 }
 
-func (r {{ .GoType }}) SetDefault(i int) {
-	switch (i) {
-	{{ range $i, $field := .Fields }}
-        {{ if .HasDefault }}
+func (r *{{ .GoType }}) SetDefault(i int) {
+	switch (i) { {{ range $i, $field := .Fields }}{{ if .HasDefault }}
 	case {{ $i }}:
-       	 	{{ $.DefaultForField $field }}
-		return
-	{{ end }}
-	{{ end }}
+		{{ $.DefaultForField $field }}
+		{{ end }}{{ end }}
+	default:
+		panic("Unknown field index")
 	}
-	panic("Unknown field index")
 }
 
-func (_ {{ .GoType }}) AppendMap(key string) types.Field { panic("Unsupported operation") }
-func (_ {{ .GoType }}) AppendArray() types.Field { panic("Unsupported operation") }
-func (_ {{ .GoType }}) Finalize() { }
+func (r *{{ .GoType }}) Clear(i int) {
+	switch (i) { {{ range $i, $f := .Fields }}{{ if $f.Type.IsOptional }}
+	case {{ $i }}:
+		r.{{ $f.GoName }} = nil{{ end }}{{ end }}
+	default:
+		panic("Non-optional field index")
+	}
+}
+
+func (_ *{{ .GoType }}) AppendMap(key string) types.Field { panic("Unsupported operation") }
+func (_ *{{ .GoType }}) ClearMap(key string) { panic("Unsupported operation") }
+func (_ *{{ .GoType }}) AppendArray() types.Field { panic("Unsupported operation") }
+func (_ *{{ .GoType }}) Finalize() { }
 `
