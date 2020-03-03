05a7b9a540fb6914405774434876a7e740d2dee2
Daniel M. Lambea <dmlambea@gmail.com>
2020-03-03 00:50:45 +0000

    Make AvroType an abstract type along with serializer method, readability check and basic default accessors

--- schema/avro_type.go
+++ schema/avro_type.go
@@ -1,17 +1,11 @@
 package schema
 
 type AvroType interface {
-	Name() string
-	GoType() string
-	IsOptional() bool
+	AbstractType
 
 	// The name of the method which writes this field onto the wire
 	SerializerMethod() string
 
-	Children() []AvroType
-
-	Attribute(name string) interface{}
-	Definition(scope map[QualifiedName]interface{}) (interface{}, error)
 	DefaultValue(lvalue string, rvalue interface{}) (string, error)
 
 	WrapperType() string
