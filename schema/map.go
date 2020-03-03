0ce651a6568cadc214dc4b78c0fe311bfb46d35b
Daniel M. Lambea <dmlambea@gmail.com>
2020-03-03 01:03:32 +0000

    Refactor Map to reduce code duplication

--- schema/map.go
+++ schema/map.go
@@ -2,19 +2,18 @@ package schema
 
 import (
 	"fmt"
-
-	"github.com/actgardner/gogen-avro/generator"
 )
 
 type MapField struct {
-	itemType   AvroType
-	definition map[string]interface{}
+	primitiveField
+	itemType AvroType
 }
 
 func NewMapField(itemType AvroType, definition map[string]interface{}) *MapField {
+	name := "Map" + itemType.Name()
 	return &MapField{
-		itemType:   itemType,
-		definition: definition,
+		primitiveField: newPrimitiveField(name, name, definition, "write"+name),
+		itemType:       itemType,
 	}
 }
 
@@ -22,42 +21,18 @@ func (s *MapField) ItemType() AvroType {
 	return s.itemType
 }
 
-func (s *MapField) Name() string {
-	return s.GoType()
-}
-
-func (s *MapField) GoType() string {
-	return "Map" + s.itemType.Name()
-}
-
-func (s *MapField) IsOptional() bool {
-	return false
-}
-
-func (s *MapField) SerializerMethod() string {
-	return fmt.Sprintf("write%v", s.Name())
-}
-
-func (s *MapField) filename() string {
-	return generator.ToSnake(s.Name()) + ".go"
-}
-
-func (s *MapField) Attribute(name string) interface{} {
-	return s.definition[name]
+func (s *MapField) Children() []AvroType {
+	return []AvroType{s.itemType}
 }
 
 func (s *MapField) Definition(scope map[QualifiedName]interface{}) (interface{}, error) {
-	def := copyDefinition(s.definition)
+	def := copyDefinition(s.definitionAsStringMap())
 	var err error
 	def["values"], err = s.itemType.Definition(scope)
 	if err != nil {
 		return nil, err
 	}
 	return def, nil
-}
-
-func (s *MapField) ConstructorMethod() string {
-	return fmt.Sprintf("New%s()", s.Name())
 }
 
 func (s *MapField) DefaultValue(lvalue string, rvalue interface{}) (string, error) {
@@ -77,26 +52,17 @@ func (s *MapField) DefaultValue(lvalue string, rvalue interface{}) (string, erro
 	return setters, nil
 }
 
-func (s *MapField) WrapperType() string {
-	return ""
-}
-
 func (s *MapField) IsReadableBy(f AvroType, visited map[QualifiedName]interface{}) bool {
-	if union, ok := f.(*UnionField); ok {
-		for _, t := range union.AvroTypes() {
-			if s.IsReadableBy(t, visited) {
-				return true
-			}
-		}
-	}
 	if reader, ok := f.(*MapField); ok {
 		return s.ItemType().IsReadableBy(reader.ItemType(), visited)
 	}
+	if s.primitiveField.IsReadableBy(f, visited) {
+		return true
+	}
+	if union, ok := f.(*UnionField); ok {
+		return isReadableByUnion(s, union, visited)
+	}
 	return false
-}
-
-func (s *MapField) SimpleName() string {
-	return s.Name()
 }
 
 func (s *MapField) ItemConstructable() string {
@@ -106,6 +72,6 @@ func (s *MapField) ItemConstructable() string {
 	return ""
 }
 
-func (s *MapField) Children() []AvroType {
-	return []AvroType{s.itemType}
+func (s *MapField) ConstructorMethod() string {
+	return fmt.Sprintf("New%s()", s.Name())
 }
