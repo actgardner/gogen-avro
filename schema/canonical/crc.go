ccbec920f337cc4999904c5bc5511ccaca8b9465
Dhia Ayachi <dhia.ayachi@bell.ca>
2020-03-20 21:18:05 -0400

    change crc to be able to read write bytes

--- schema/canonical/crc.go
+++ schema/canonical/crc.go
@@ -3,37 +3,18 @@ package canonical
 import (
 	"bytes"
 	"encoding/binary"
-	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"github.com/actgardner/gogen-avro/parser"
+	"github.com/actgardner/gogen-avro/resolver"
+	"io"
 )
-
-// IMplementation of Avro's 64-bit Rabin code for fingerprinting schemas
-
-/*
-long fingerprint64(byte[] buf) {
-  if (FP_TABLE == null) initFPTable();
-  long fp = EMPTY;
-  for (int i = 0; i < buf.length; i++)
-    fp = (fp >>> 8) ^ FP_TABLE[(int)(fp ^ buf[i]) & 0xff];
-  return fp;
-}
-
-static long EMPTY = 0xc15d213aa4d7a795L;
-static long[] FP_TABLE = null;
-
-void initFPTable() {
-  FP_TABLE = new long[256];
-  for (int i = 0; i < 256; i++) {
-    long fp = i;
-    for (int j = 0; j < 8; j++)
-      fp = (fp >>> 1) ^ (EMPTY & -(fp & 1L));
-    FP_TABLE[i] = fp;
-  }
-}
-*/
 
 var FP_TABLE []uint64
 
 const EMPTY uint64 = 0xc15d213aa4d7a795
+
+var HeaderV1 = []byte{0xC3, 0x01}
 
 func init() {
 	FP_TABLE = make([]uint64, 256)
@@ -46,12 +27,53 @@ func init() {
 	}
 }
 
-func AvroCRC64Fingerprint(data []byte) string {
+func AvroCRC64Fingerprint(schema []byte, writer io.Writer) error {
 	fp := EMPTY
-	for _, d := range data {
+	for _, d := range schema {
 		fp = (fp >> 8) ^ FP_TABLE[(fp^uint64(d))&0xff]
 	}
-	output := bytes.NewBuffer(make([]byte, 0, 8))
-	binary.Write(output, binary.LittleEndian, fp)
-	return hex.EncodeToString(output.Bytes())
+	err := binary.Write(writer, binary.LittleEndian, fp)
+	return err
+}
+
+func AvroCalcSchemaUID(schema string) []byte {
+	b := make([]byte, 0, 8)
+	output := bytes.NewBuffer(b)
+	ns := parser.NewNamespace(false)
+
+	s, err := ns.TypeForSchema([]byte(schema))
+	if err != nil {
+		panic("Invalid schema 1")
+	}
+	for _, def := range ns.Roots {
+		err = resolver.ResolveDefinition(def, ns.Definitions)
+		if err != nil {
+			panic("Invalid schema 2")
+		}
+	}
+	canonical, err := json.Marshal(CanonicalForm(s))
+	err = AvroCRC64Fingerprint(canonical, output)
+	if err != nil {
+		panic("Invalid schema 1")
+	}
+	return output.Bytes()
+}
+
+func AvroVersionHeader(writer io.Writer, header []byte) error {
+	fp := HeaderV1
+	err := binary.Write(writer, binary.LittleEndian, fp)
+	if err != nil {
+		return err
+	}
+	err = binary.Write(writer, binary.LittleEndian, header)
+	return err
+}
+
+func AvroConsumeHeader(r io.Reader) error {
+	buf := make([]byte, 10)
+	n, err := r.Read(buf)
+	if n < 10 || err != nil {
+		return errors.New("message length <2")
+	}
+	return nil
 }
--- schema/canonical/crc_test.go
+++ schema/canonical/crc_test.go
@@ -1,24 +1,55 @@
 package canonical
 
 import (
-	"testing"
-
+	"bytes"
+	"encoding/hex"
+	"encoding/json"
+	"github.com/actgardner/gogen-avro/example/avro"
+	"github.com/actgardner/gogen-avro/parser"
+	"github.com/actgardner/gogen-avro/resolver"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestAvroCRC64Fingerprint(t *testing.T) {
+	m := avro.NewDemoSchema().Schema()
 	cases := []struct {
 		schema      string
 		fingerprint string
 	}{
-		{`"int"`, "8f5c393f1ad57572"},
-		{`"float"`, "90d7a83ecb027c4d"},
-		{`"long"`, "b71df49344e154d0"},
-		{`"double"`, "7e95ab32c035758e"},
-		{`"bytes"`, "651920c3da16c04f"},
+		{m, "0c947f601de7ce84"},
+		{"{\"type\":\"record\",\"name\":\"SmsMessage\",\"namespace\":\"ca.bell.vas.crossfunctional.domain\",\"fields\":[{\"name\":\"versionNumber\",\"type\":{\"type\":\"string\",\"avro.java.string\":\"String\"},\"default\":\"\"},{\"name\":\"transactionType\",\"type\":{\"type\":\"enum\",\"name\":\"TransactionType\",\"symbols\":[\"MSG\",\"ACK\",\"RSP\"]},\"default\":\"MSG\"},{\"name\":\"orignalShortMessageInMsgPayload\",\"type\":\"boolean\",\"default\":false},{\"name\":\"startTimestamp\",\"type\":\"long\",\"default\":0},{\"name\":\"internalTransactionId\",\"type\":{\"type\":\"string\",\"avro.java.string\":\"String\"},\"default\":\"\"},{\"name\":\"externalTransactionId\",\"type\":{\"type\":\"string\",\"avro.java.string\":\"String\"},\"default\":\"\"},{\"name\":\"queueResponseName\",\"type\":{\"type\":\"string\",\"avro.java.string\":\"String\"},\"default\":\"\"},{\"name\":\"responseSequenceNumber\",\"type\":\"int\",\"default\":0},{\"name\":\"messageSource\",\"type\":{\"type\":\"string\",\"avro.java.string\":\"String\"},\"default\":\"\"},{\"name\":\"messageDestination\",\"type\":{\"type\":\"enum\",\"name\":\"TransactionDestination\",\"symbols\":[\"BELL\",\"LONGCODE\",\"ICMS\",\"ROGERS\",\"SYNIVERSE\",\"EXT\",\"UNKNOWN\"]},\"default\":\"UNKNOWN\"},{\"name\":\"messageDestinationCode\",\"type\":{\"type\":\"string\",\"avro.java.string\":\"String\"},\"default\":\"\"},{\"name\":\"segmentSequenceNumber\",\"type\":{\"type\":\"int\",\"java-class\":\"java.lang.Short\"},\"default\":0},{\"name\":\"totalNumberOfSegment\",\"type\":{\"type\":\"int\",\"java-class\":\"java.lang.Short\"},\"default\":0},{\"name\":\"segmentMessageRefNumber\",\"type\":{\"type\":\"int\",\"java-class\":\"java.lang.Short\"},\"default\":0},{\"name\":\"contentFilteringResult\",\"type\":{\"type\":\"enum\",\"name\":\"ContentFilteringResult\",\"symbols\":[\"BLOCKED\",\"ALLOWED\",\"NULL\"]},\"default\":\"ALLOWED\"},{\"name\":\"pdu\",\"type\":{\"type\":\"record\",\"name\":\"Pdu\",\"fields\":[{\"name\":\"name\",\"type\":{\"type\":\"string\",\"avro.java.string\":\"String\"},\"default\":\"\"},{\"name\":\"isRequest\",\"type\":\"boolean\",\"default\":false},{\"name\":\"commandLength\",\"type\":\"int\",\"default\":0},{\"name\":\"commandId\",\"type\":\"int\",\"default\":4},{\"name\":\"commandStatus\",\"type\":\"int\",\"default\":0},{\"name\":\"sequenceNumber\",\"type\":\"int\",\"default\":0},{\"name\":\"serviceType\",\"type\":{\"type\":\"string\",\"avro.java.string\":\"String\"},\"default\":\"\"},{\"name\":\"sourceAddress\",\"type\":{\"type\":\"record\",\"name\":\"Address\",\"fields\":[{\"name\":\"ton\",\"type\":{\"type\":\"int\",\"java-class\":\"java.lang.Byte\"},\"default\":0},{\"name\":\"npi\",\"type\":{\"type\":\"int\",\"java-class\":\"java.lang.Byte\"},\"default\":0},{\"name\":\"address\",\"type\":{\"type\":\"string\",\"avro.java.string\":\"String\"},\"default\":\"\"}]},\"default\":{}},{\"name\":\"destAddress\",\"type\":\"Address\",\"default\":{}},{\"name\":\"esmClass\",\"type\":{\"type\":\"int\",\"java-class\":\"java.lang.Byte\"},\"default\":0},{\"name\":\"protocolId\",\"type\":{\"type\":\"int\",\"java-class\":\"java.lang.Byte\"},\"default\":0},{\"name\":\"priority\",\"type\":{\"type\":\"int\",\"java-class\":\"java.lang.Byte\"},\"default\":0},{\"name\":\"scheduleDeliveryTime\",\"type\":{\"type\":\"string\",\"avro.java.string\":\"String\"},\"default\":\"\"},{\"name\":\"validityPeriod\",\"type\":{\"type\":\"string\",\"avro.java.string\":\"String\"},\"default\":\"\"},{\"name\":\"registeredDelivery\",\"type\":{\"type\":\"int\",\"java-class\":\"java.lang.Byte\"},\"default\":0},{\"name\":\"replaceIfPresent\",\"type\":{\"type\":\"int\",\"java-class\":\"java.lang.Byte\"},\"default\":0},{\"name\":\"dataCoding\",\"type\":{\"type\":\"int\",\"java-class\":\"java.lang.Byte\"},\"default\":0},{\"name\":\"defaultMsgId\",\"type\":{\"type\":\"int\",\"java-class\":\"java.lang.Byte\"},\"default\":0},{\"name\":\"shortMessage\",\"type\":{\"type\":\"bytes\",\"java-class\":\"[B\"},\"default\":\"ÿ\"},{\"name\":\"optionalParameters\",\"type\":{\"type\":\"array\",\"items\":{\"type\":\"record\",\"name\":\"Tlv\",\"fields\":[{\"name\":\"tag\",\"type\":{\"type\":\"int\",\"java-class\":\"java.lang.Short\"},\"default\":0},{\"name\":\"value\",\"type\":{\"type\":\"bytes\",\"java-class\":\"[B\"},\"default\":\"ÿ\"},{\"name\":\"tagName\",\"type\":{\"type\":\"string\",\"avro.java.string\":\"String\"},\"default\":\"\"}]},\"java-class\":\"java.util.ArrayList\"},\"default\":[]}]},\"default\":{}}],\"default\":{}}", "0c947f601de7ce84"},
 	}
 
 	for _, c := range cases {
-		assert.Equal(t, c.fingerprint, AvroCRC64Fingerprint([]byte(c.schema)))
+		b := make([]byte, 0, 8)
+		output := bytes.NewBuffer(b)
+		ns := parser.NewNamespace(false)
+		s, err := ns.TypeForSchema([]byte(c.schema))
+		assert.Nil(t, err)
+		for _, def := range ns.Roots {
+			assert.Nil(t, resolver.ResolveDefinition(def, ns.Definitions))
+		}
+		canonical, err := json.Marshal(CanonicalForm(s))
+		err = AvroCRC64Fingerprint(canonical, output)
+		assert.Nil(t, err)
+		assert.Equal(t, c.fingerprint, hex.EncodeToString(output.Bytes()))
+	}
+}
+
+func TestAvroVersionHeader(t *testing.T) {
+	cases := []struct {
+		header   []byte
+		expected string
+	}{
+		{avro.NewDemoSchema().Schema(), "c3010c947f601de7ce84"},
+	}
+
+	for _, c := range cases {
+		b := make([]byte, 0, 2)
+		output := bytes.NewBuffer(b)
+		err := AvroVersionHeader(output, c.header)
+		assert.Nil(t, err)
+		assert.Equal(t, c.expected, hex.EncodeToString(output.Bytes()))
 	}
 }
