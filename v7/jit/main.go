441537b87cba9d2ecce455f602451ad9cd8bbaab
A Gardner <3100188+actgardner@users.noreply.github.com>
2020-08-15 17:34:51 -0400

    Working method calls with no args

--- v7/jit/main.go
+++ v7/jit/main.go
@@ -1,42 +1,92 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
+	"reflect"
 	"syscall"
 	"unsafe"
 )
 
-type printFunc func()
+func PrintHello() {
+	fmt.Printf("Hello World!\n")
+}
 
-func main() {
-	printFunction := []uint16{
-		0xb804, 0x0000, 0x0290, // movl $0x2000004,%eax
-		0xbf01, 0x0000, 0x0090, // movl $0x1,%edi
-		0xba0d, 0x0000, 0x0090, // mov $0x13, %rdx
-		0x8d35, 0x0400, 0x0000, // lea 0x4(%rip), %rsi
-		0xf05,                  // syscall
-		0xc3cc,                 // ret
-		0x4865, 0x6c6c, 0x6f20, // Hello_(whitespace)
-		0x576f, 0x726c, 0x6421, 0xa, // World!
-	}
-	executablePrintFunc, err := syscall.Mmap(
+type program struct {
+	ops    []byte
+	offset int
+}
+
+func newProgram(size int) (*program, error) {
+	executableFunc, err := syscall.Mmap(
 		-1,
 		0,
-		128,
+		size,
 		syscall.PROT_READ|syscall.PROT_WRITE|syscall.PROT_EXEC,
 		syscall.MAP_PRIVATE|syscall.MAP_ANON)
 	if err != nil {
-		fmt.Printf("mmap err: %v", err)
+		return nil, err
+	}
+	return &program{executableFunc, 0}, nil
+}
+
+func (p *program) appendBytes(ops ...byte) {
+	copy(p.ops[p.offset:], ops)
+	p.offset += len(ops)
+}
+
+func (p *program) call(addr uintptr) {
+	// Grow the stack by 8 (subq	$0x8, %rsp)
+	p.appendBytes(0x48, 0x83, 0xec, 0x08)
+
+	// Copy BP to the stack (movq	%rbp, (%rsp))
+	p.appendBytes(0x48, 0x89, 0x2c, 0x24)
+
+	// Load new RBP (leaq	(%rsp), %rbp)
+	p.appendBytes(0x48, 0x8d, 0x2c, 0x24)
+
+	// Calculate the jump relative to the next instruction and encode it as 32-bit signed int
+	rip := uintptr(unsafe.Pointer(&p.ops[0])) + uintptr(p.offset) + 5
+	addrDiff := int32(int64(addr) - int64(rip))
+	binary.LittleEndian.PutUint32(p.ops[p.offset+1:], *((*uint32)(unsafe.Pointer(&addrDiff))))
+	p.ops[p.offset] = 0xe8
+
+	p.offset += 5
+
+	// movq	(%rsp), %rbp
+	p.appendBytes(0x48, 0x8b, 0x2c, 0x24)
+
+	// addq	$0x8, %rsp
+	p.appendBytes(0x48, 0x83, 0xc4, 0x08)
+}
+
+func (p *program) int3() {
+	p.ops[p.offset] = 0xcc
+	p.offset += 1
+}
+
+func (p *program) ret() {
+	p.ops[p.offset] = 0xc3
+	p.ops[p.offset+1] = 0xcc
+	p.offset += 2
+}
+
+func (p *program) funcPtr() unsafe.Pointer {
+	prog := uintptr(unsafe.Pointer(&p.ops))
+	return unsafe.Pointer(&prog)
+}
+
+func main() {
+	p, err := newProgram(128)
+	if err != nil {
+		fmt.Printf("mmap error: %v", err)
 		return
 	}
 
-	for i := range printFunction {
-		executablePrintFunc[i*2] = byte(printFunction[i] >> 8)
-		executablePrintFunc[i*2+1] = byte(printFunction[i])
-	}
-
-	type printFunc func()
-	unsafePrintFunc := (uintptr)(unsafe.Pointer(&executablePrintFunc))
-	printer := *(*printFunc)(unsafe.Pointer(&unsafePrintFunc))
-	printer()
+	p.call(reflect.ValueOf(PrintHello).Pointer())
+	//p.int3()
+	p.ret()
+	fn := p.funcPtr()
+	exeFn := *(*func())(fn)
+	exeFn()
 }
